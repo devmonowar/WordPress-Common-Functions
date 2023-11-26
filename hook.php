d5adb68f67fff6558b813d05b44671cc950dfb04
kstmonowar <kstmonowar@gmail.com>
2023-11-26 08:42:15 +0600

    add filter hook

--- hook.php
+++ hook.php
@@ -1,9 +1,9 @@
 <?php
 
-do_action( ), add_action( );
+do_action( ); add_action( );
 
 // function file
--------------------------
+// -------------------------
 
 add_action( 'test_post', 'test_add_post');
 
@@ -13,6 +13,27 @@ function test_add_post() {
 
 
 // another file (je khane show korabo)
--------------------------------
+// -------------------------------
 
 do_action( 'test_post');
+
+// =======================================
+
+apply_filters(); add_filters();
+
+// function.php file---
+// -------------------------------
+
+add_filter( 'ehitahobe', 'function_name' );
+
+function function_name($def){
+    echo "ehi ta add korlam" . $def ;
+}
+
+// --
+// another file (jekhane change kora hobe)
+// --------------------------
+
+$value = "<h3> ekhane aro kicu add korbo </h3>";
+
+echo apply_filters( "ehitahobe", $value );
